3817aaf7f2841bb4058e95cc4d2aa279e4470ffa
Justin Taj <justintaj@yahoo.com>
2019-03-18 16:51:11 -0700

    removed HEAD text from controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -14,11 +14,11 @@ class HomeController < ApplicationController
     @email = params[:email]
     @message = params[:message]
     ActionMailer::Base.mail(from: @email,
-<<<<<<< HEAD
+
         to: 'justintaj@yahoo.com.com',
-=======
+
         to: 'justintaj@yahoo.com',
->>>>>>> 3151a5a61987bd94a5b8595f84886733ca3bd835
+
         subject: "A new contact form message from #{@name}",
         body: @message).deliver_now
   end
